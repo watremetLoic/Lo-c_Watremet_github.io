a59cb39c412f222fa62d95713865b791c2905729
loic <loic.watremet@student.vives.be>
2023-03-03 23:43:59 +0100

    Labo 2

---
+++ SEMESTER 2/Labos/labo2/Opdracht kopieer/scripts/code.js
@@ -0,0 +1,12 @@
+const setup = () => {
+
+    let btnKopieer = document.getElementById("btnKopieer");
+    btnKopieer.addEventListener("click", kopieer);
+    const kopieer = () => {
+        let txtInput = document.getElementById("txtInput");
+        let tekst = txtInput.value;
+        let txtOutput = document.getElementById("txtOutput");
+        txtOutput.innerHTML = tekst;
+    }
+}
+window.addEventListener("load", setup);
